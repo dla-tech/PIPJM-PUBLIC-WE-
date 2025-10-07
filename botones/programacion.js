72543df61eef81fdf9f02ec16583a7cdbbcad93b
dla-tech <darroyolebarro@hotmail.com>
2025-10-07 09:24:31 -0400

    Actualizar programacion.js

--- botones/programacion.js
+++ botones/programacion.js
@@ -23,6 +23,7 @@ document.getElementById("content").innerHTML = `
       padding: 0 10px;
       box-sizing: border-box;
     ">
+      <h2 style="margin-bottom: 15px;">Pastora: Nelida Brito Morales Copastor: Marcos River</h2>
       <h2 style="margin-bottom: 20px;">📅 Programación General de Cultos</h2>
       <p><strong>Lunes:</strong> Culto de Oración — 7:00 p.m. en el templo</p>
       <p><strong>Martes:</strong> Culto Evangelístico — 7:00 p.m. en las calles</p>
